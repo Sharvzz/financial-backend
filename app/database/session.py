efb4828933d886e3ac83caa6af4cecc36ce6f4ab
Sharvzz <loganathansharvesh14@gmail.com>
2025-08-26 13:32:09 +0530

    updated

--- app/database/session.py
+++ app/database/session.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:CmpkJJZPavJSwCUYfPaNqGabzaHJPmFz@tramway.proxy.rlwy.net:54731/railway")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
